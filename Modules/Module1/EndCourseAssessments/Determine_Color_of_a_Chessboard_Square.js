942cd967744ca1e45cf177373e51e5c592ae37cc
Praveen Katti <praveendk25@gmail.com>
2024-11-18 00:57:12 +0530

    wrote solution

--- Modules/Module1/EndCourseAssessments/Determine_Color_of_a_Chessboard_Square.js
+++ Modules/Module1/EndCourseAssessments/Determine_Color_of_a_Chessboard_Square.js
@@ -12,3 +12,17 @@ Explanation:
 From the chessboard above, the square with coordinates "h3" is white, so return true. */
 
 
+// Do not remove nor make any changes in  main() function
+function main() {
+    var coordinates = readLine();
+    console.log(squareIsWhite(coordinates));
+}
+var squareIsWhite = function(coordinates) {
+  // Your code goes here
+  let letter = ["a","b","c","d","e","f","g","h"]
+  const [l,n] = coordinates.split("")
+  if(letter.indexOf(l)%2==0&&n%2==0||letter.indexOf(l)%2==1&&n%2==1){
+    return true
+  }
+  return false
+};
