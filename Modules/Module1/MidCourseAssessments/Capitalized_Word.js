36de53169ffdc24f86185847b7f5990bd982c0f4
Praveen Katti <praveendk25@gmail.com>
2024-11-17 23:16:59 +0530

    added aim and exmaple

---
+++ Modules/Module1/MidCourseAssessments/Capitalized_Word.js
@@ -0,0 +1,12 @@
+/* write a function that will return a new string with each word’s first letter capitalized. */
+
+/* Example 1:
+Input:
+hello world
+
+Output:
+Hello World
+
+Explanation:
+Each word’s first letter capitalized. */
+
