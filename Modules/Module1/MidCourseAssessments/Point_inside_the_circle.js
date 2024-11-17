13ede7e9ee2260f0f3802a61d52291d291d320c7
Praveen Katti <praveendk25@gmail.com>
2024-11-18 00:10:43 +0530

    added aim of problem and example

---
+++ Modules/Module1/MidCourseAssessments/Point_inside_the_circle.js
@@ -0,0 +1,17 @@
+/* write a function that takes the circle's center coordinates, radius, and a point's coordinates as input. 
+It should return true if the point lies strictly inside the circle and false otherwise. */
+
+/* Example 1:
+Input:
+Center of the circle: (0, 0)
+Radius of circle: 5
+Point inside the circle: (2, 2)
+
+Output:
+true
+
+Explanation:
+The distance between the point (2, 2) and the circle's center (0, 0) is less than the radius 5. 
+Therefore, the point lies strictly inside the circle. */
+
+
