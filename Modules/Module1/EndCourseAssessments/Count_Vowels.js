c4208040e2b526ab63b797c1f26d4319aaf6c2c8
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:32:29 +0530

    added aim of problem and example

---
+++ Modules/Module1/EndCourseAssessments/Count_Vowels.js
@@ -0,0 +1,14 @@
+/* write a JavaScript function that takes a string as input and returns the total count of vowels (a, e, i, o, u) 
+present in the string. The function should consider both uppercase and lowercase vowels. */
+
+/* Example 1:
+Input:
+str = "Hello World!"
+
+Output:
+3
+
+Explanation:
+The vowels in the string are e, o, and o, totaling 3 vowels. */
+
+
