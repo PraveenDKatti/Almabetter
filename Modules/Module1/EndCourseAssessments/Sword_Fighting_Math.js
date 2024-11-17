e0c906eb651a59387676b2bf4e31b0e837b130dc
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:34:45 +0530

    added aim of problem and example

---
+++ Modules/Module1/EndCourseAssessments/Sword_Fighting_Math.js
@@ -0,0 +1,14 @@
+/* write a JavaScript function that takes an array of positive integers (representing sword lengths) and 
+returns the number of unique triangles that can be formed from them.
+For any three lengths, a, b, and c (where a <= b <= c), a triangle can be formed if: a + b > c */
+
+/* Example 1:
+Input:
+arr = [3, 5, 4]; n = 3;
+
+Output:
+1
+
+Explanation:
+The only triangle possible is formed by using lengths 3, 4, and 5. */
+
