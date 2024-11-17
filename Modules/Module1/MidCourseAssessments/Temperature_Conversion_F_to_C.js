51f766ba0c19472fb58b68ea429704dfa8994f5c
Praveen Katti <praveendk25@gmail.com>
2024-11-18 00:14:30 +0530

    wrote solution

--- Modules/Module1/MidCourseAssessments/Temperature_Conversion_F_to_C.js
+++ Modules/Module1/MidCourseAssessments/Temperature_Conversion_F_to_C.js
@@ -11,3 +11,12 @@ Output:
 Explanation:
 The temperature 90°F converts to (90 - 32) * 5/9 ≈ 32.22°C. */
 
+//  Do not remove nor make any changes in  main() function
+function main() {
+    var a = parseInt(readLine());
+    console.log(fahrenheitToCelsius(a));
+}
+function fahrenheitToCelsius(fahrenheit) {
+  // Your code goes here
+  return (fahrenheit-32)*5/9
+}
