587ad87d91a2f9b56f2355f2bea475fed7a35c3e
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:08:35 +0530

    wrote solution

--- Modules/Module1/EndCourseAssessments/Compress_Video_Frames.js
+++ Modules/Module1/EndCourseAssessments/Compress_Video_Frames.js
@@ -9,3 +9,21 @@ Output:
 3 */
 
 
+// Do not remove nor make any changes in  main() function
+function main() {
+    var frames = readLine().split(" ").map(x => parseInt(x));
+    console.log(compressVideo(frames));
+}
+function compressVideo(frames) {
+  // Your code goes here
+  let i=0
+  while(i<frames.length){
+    if((i>0&&frames[i]==frames[i-1])||(i<frames.length-1&&frames[i]==frames[i+1])){
+        frames.splice(i,1)
+        i=Math.max(i-1,0)
+    }
+    else
+        i++
+  }
+  return frames.length
+}
