ef5d1e58d07f5fb01eb257e48b778b44db95c09f
Praveen Katti <praveendk25@gmail.com>
2024-11-17 20:25:06 +0530

    added solution

--- Modules/Module1/MidCourseAssessments/Area_of_Triangle.js
+++ Modules/Module1/MidCourseAssessments/Area_of_Triangle.js
@@ -10,6 +10,19 @@ Output:
 
 Explanation:
 Using Heron's formula, s = (5 + 6 + 7) / 2 = 9.
-
 Then, Area = √(9 * (9 - 5) * (9 - 6) * (9 - 7)) ≈ 14.70. */
 
+
+// Do not remove nor make any changes in  main() function
+function main() {
+    var side1 = parseInt(readLine());
+    var side2 = parseInt(readLine());
+    var side3 = parseInt(readLine());
+    console.log(getTriangleArea(side1, side2, side3));
+}
+function getTriangleArea(side1, side2, side3) {
+  // Your code goes here
+  let sp = 0.5*(side1+side2+side3)
+  let area = Math.sqrt(sp*(sp-side1)*(sp-side2)*(sp-side3))
+  return area
+}
