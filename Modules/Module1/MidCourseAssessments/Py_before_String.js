fda8431e88fc335510be2bd86ac55892eb6357f1
Praveen Katti <praveendk25@gmail.com>
2024-11-17 23:27:47 +0530

    wrote solution

--- Modules/Module1/MidCourseAssessments/Py_before_String.js
+++ Modules/Module1/MidCourseAssessments/Py_before_String.js
@@ -10,3 +10,15 @@ Python
 Explanation:
 "Py" added as it's not already prefixed. */
 
+// Do not remove nor make any changes in  main() function
+function main() {
+    var str = readLine();
+    console.log(addPy(str));
+}
+function addPy(str) {
+  // Your code goes here
+  if(str.startsWith("Py"))
+    return str
+    else
+    return "Py"+str
+}
