6a2be17a6e1bce0b9645a8bd29fde0df22d2404d
Praveen Katti <praveendk25@gmail.com>
2024-11-17 20:24:00 +0530

    added aim and example

---
+++ Modules/Module1/MidCourseAssessments/Area_of_Triangle.js
@@ -0,0 +1,15 @@
+/* Calculate and return the area of triangle using Heron’s formula. The function should return 
+the area of the triangle as a floating-point number. */
+
+/* Example 1:
+Input:
+side1 = 5, side2 = 6, side3 = 7
+
+Output:
+14.696938456699069
+
+Explanation:
+Using Heron's formula, s = (5 + 6 + 7) / 2 = 9.
+
+Then, Area = √(9 * (9 - 5) * (9 - 6) * (9 - 7)) ≈ 14.70. */
+
