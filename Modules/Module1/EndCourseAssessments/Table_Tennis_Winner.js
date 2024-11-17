7cb944f7023a4ddbf9a4f22d05a0a8e492bb496d
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:26:14 +0530

    added aim of problem and example

---
+++ Modules/Module1/EndCourseAssessments/Table_Tennis_Winner.js
@@ -0,0 +1,17 @@
+/* A game shall be won by the player first scoring 11 points except in the case when both players 
+have 10 points each, then the game shall be won by the last player who scored last point.
+return WIN if player reaches the condition, else return LOSE. */
+
+/* 1 represents players's point. 0 represents Opponent's point. */
+
+/* Example 1:
+Input:
+11111111110000000000
+
+Output:
+LOSE
+
+Explanation:
+Both players reach 10 points each. Opponent scores the final point, gaining a 2-point lead. */
+
+
