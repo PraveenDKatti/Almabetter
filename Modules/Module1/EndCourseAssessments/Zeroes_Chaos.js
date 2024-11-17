adb4feed85be262233bebdefbc11ed4577fad414
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:11:40 +0530

    added aim of problem and example

---
+++ Modules/Module1/EndCourseAssessments/Zeroes_Chaos.js
@@ -0,0 +1,10 @@
+/* Write a function to push all the zeros to the end of the array, while keeping the order of 
+all other elements the same. */
+
+/* Example 1:
+Input:
+arr[] = {1, 2, 0, 4, 3, 0, 5, 0};
+
+Output:
+[1, 2, 4, 3, 5, 0, 0, 0] */
+
