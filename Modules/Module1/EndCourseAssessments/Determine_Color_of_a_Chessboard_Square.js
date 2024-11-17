2e908179d174b0f6b86b3b6edfa1ed93760bd56a
Praveen Katti <praveendk25@gmail.com>
2024-11-18 00:56:39 +0530

    added aim of problem and example

---
+++ Modules/Module1/EndCourseAssessments/Determine_Color_of_a_Chessboard_Square.js
@@ -0,0 +1,14 @@
+/* Given a string that represents the coordinates of a square of the chessboard. Coordinates start with letter 
+ends with digit. Return true if square is white, and false if square is black. */
+
+/* Example 2:
+Input:
+coordinates = "h3"
+
+Output:
+true
+
+Explanation:
+From the chessboard above, the square with coordinates "h3" is white, so return true. */
+
+
