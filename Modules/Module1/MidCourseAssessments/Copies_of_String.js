c39092132351948f568e69ebe2c6c467edb277f5
Praveen Katti <praveendk25@gmail.com>
2024-11-18 00:04:26 +0530

    added aim of problem and example

---
+++ Modules/Module1/MidCourseAssessments/Copies_of_String.js
@@ -0,0 +1,14 @@
+/* Write a function that take a string and a positive number as inputs and return a new string 
+with the original string repeated the specified number of times. */
+
+/* Example 1:
+Input:
+str = "hello";
+num = 3;
+
+Output:
+hellohellohello
+
+Explanation:
+The string "hello" is repeated 3 times, resulting in "hellohellohello". */
+
