f2f247c8fc6268eb12fc46b3bb7c0e8e1b55d989
Praveen Katti <praveendk25@gmail.com>
2024-11-17 23:26:43 +0530

    added comment aim of problem and example

---
+++ Modules/Module1/MidCourseAssessments/Py_before_String.js
@@ -0,0 +1,12 @@
+/* Add the prefix to the string with "Py", if it's not already prefixed. */
+
+/* Example 1:
+Input:
+thon
+
+Output:
+Python
+
+Explanation:
+"Py" added as it's not already prefixed. */
+
