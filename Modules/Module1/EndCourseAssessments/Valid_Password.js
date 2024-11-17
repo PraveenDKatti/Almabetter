e869c7189eecca54adaa2a365e7319713277c1a0
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:20:06 +0530

    wrote solution

--- Modules/Module1/EndCourseAssessments/Valid_Password.js
+++ Modules/Module1/EndCourseAssessments/Valid_Password.js
@@ -7,3 +7,14 @@ Abc123!@
 Output:
 true */
 
+
+// Do not remove nor make any changes in  main() function
+function main() {
+    var password = readLine();
+    console.log(isValidPassword(password));
+}
+function isValidPassword(password) {
+  //Your code goes here
+  let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*_+])[a-zA-Z0-9!@#$%^&*_+]{8,}$/
+  return regex.test(password)
+}
