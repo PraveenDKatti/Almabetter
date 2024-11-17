4c42ca1e6fccc1d48765e567c125b0e1c43b913d
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:29:32 +0530

    wrote solution

--- Modules/Module1/EndCourseAssessments/Valid_Phone_Number.js
+++ Modules/Module1/EndCourseAssessments/Valid_Phone_Number.js
@@ -13,4 +13,12 @@ Explanation:
 The number starts with 9, contains exactly 10 digits, and meets all the validation requirements. */
 
 
-
+// Do not remove nor make any changes in  main() function
+function main() {
+    var phoneNumber = parseInt(readLine());
+    console.log(isValidPhoneNumber(phoneNumber));
+}
+function isValidPhoneNumber(phoneNumber) {
+  // Your code goes here
+  return /^[789][0-9]{9}$/.test(phoneNumber)
+}
