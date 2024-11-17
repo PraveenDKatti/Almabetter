6d746c2ac4eb22d9cdcb2d226c4160d36306a91f
Praveen Katti <praveendk25@gmail.com>
2024-11-17 23:36:08 +0530

    added aim of problem and example

---
+++ Modules/Module1/MidCourseAssessments/Temperature_Conversion_C_to_F.js
@@ -0,0 +1,13 @@
+/* write a JavaScript function that will convert a given temperature from Celsius to Fahrenheit
+Use the formula to convert Celsius to Fahrenheit: Fahrenheit = (Celsius * 9/5) + 32. */
+
+/* Example 1:
+Input:
+celsius = 25
+
+Output:
+77
+
+Explanation:
+The temperature 25°C converts to (25 * 9/5) + 32 = 77°F. */
+
