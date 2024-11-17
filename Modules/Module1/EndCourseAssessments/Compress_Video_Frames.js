fad7a275c0caff0b31b16d804f1bbd64645ad7cb
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:07:58 +0530

    added aim of problem and example

---
+++ Modules/Module1/EndCourseAssessments/Compress_Video_Frames.js
@@ -0,0 +1,11 @@
+/* Video has N frames Choose an index i (frame) if value of the i'th frame is equal to either of its 
+neighbors, remove the i'th frame. Find the minimum number of frames. */
+
+/* Example 1:
+Input:
+frames = [2 1 2 2]
+
+Output:
+3 */
+
+
