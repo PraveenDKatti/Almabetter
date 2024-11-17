3979806ed01d3b6fc5af298dc2387fc1aebb9357
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:12:13 +0530

    wrote solution

--- Modules/Module1/EndCourseAssessments/Zeroes_Chaos.js
+++ Modules/Module1/EndCourseAssessments/Zeroes_Chaos.js
@@ -8,3 +8,22 @@ arr[] = {1, 2, 0, 4, 3, 0, 5, 0};
 Output:
 [1, 2, 4, 3, 5, 0, 0, 0] */
 
+
+// Do not remove nor make any changes in  main() function
+function main() {
+    var arr = readLine().split(" ").map(x => parseInt(x));
+    console.log(pushZerosToEnd(arr));
+}
+function pushZerosToEnd(arr) {
+    // Your code goes here
+    let zeroes = 0
+    for(i=0;i<arr.length;i++){
+        if(arr[i]!=0){
+            arr[zeroes++]=arr[i]
+        }
+    }
+    for(i=zeroes;i<arr.length;i++){
+        arr[i]=0
+    }
+    return arr
+}
