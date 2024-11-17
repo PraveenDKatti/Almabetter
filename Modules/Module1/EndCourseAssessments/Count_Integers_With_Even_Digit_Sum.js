a93276d1c8329de1a3f9565d6b5cf316f45db8a4
Praveen Katti <praveendk25@gmail.com>
2024-11-18 00:21:34 +0530

    added aim of problem and example

---
+++ Modules/Module1/EndCourseAssessments/Count_Integers_With_Even_Digit_Sum.js
@@ -0,0 +1,14 @@
+/* Write a function to return the number of positive integers less than or equal to num 
+whose digit sums are even. */
+
+/* Example 2:
+Input:
+num = 30
+
+Output:
+14
+
+Explanation:
+The 14 integers less than or equal to 30 whose digit sums are even are 2, 4, 6, 8, 11, 
+13, 15, 17, 19, 20, 22, 24, 26, and 28. */
+
