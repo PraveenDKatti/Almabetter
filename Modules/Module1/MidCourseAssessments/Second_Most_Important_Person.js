e54cf57961fa17b454978e72d1a16004a8fbc186
Praveen Katti <praveendk25@gmail.com>
2024-11-17 17:39:01 +0530

    added aim of problem and example

--- Modules/Module1/MidCourseAssessments/Second_Most_Important_Person.js
+++ Modules/Module1/MidCourseAssessments/Second_Most_Important_Person.js
@@ -1,3 +1,14 @@
+/* Write a program to find the second largest number in a, b, c to find the 
+second most important among A, B, C.
+ */
+
+/*  Example 1:
+Input:
+a = 15 b=20 c=10
+
+Output:
+15 */
+
 // Do not remove nor make any changes in  main() function
 function main() {
     var a = parseInt(readLine());
@@ -5,7 +16,10 @@ function main() {
     var c = parseInt(readLine());
     console.log(secondImportant(a,b,c));
 }
+
+//Solution
 const secondImportant = function(a,b,c){
    // Your code goes here
    return [a,b,c].sort()[1]
 };
+
