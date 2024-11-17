1f2db21cca7f6736bf66e99b7dadeb607387cd69
Praveen Katti <praveendk25@gmail.com>
2024-11-17 23:56:26 +0530

    added aim of problem and example

---
+++ Modules/Module1/MidCourseAssessments/String_Shifting_Mystery.js
@@ -0,0 +1,21 @@
+/* Write a JavaScript program to compare Left Shift(s) and Right Shift(s) to see if they are equal.
+If it does, output "YES", otherwise output "NO".
+
+Left Shift (L): Moves all characters in the string one step to the left, 
+            placing the first character at the end.
+Right Shift (R): Moves all characters in the string one step to the right, 
+            placing the last character at the beginning. */
+
+
+/* Example 2:
+Input:
+s = "abcd"
+            
+Output:
+NO
+            
+Explanation:
+For s = "abcd", the left shift yields "bcda" and the right shift yields "dabc", which are different. 
+Thus, there is no V for which both shifts yield S.
+ */
+
