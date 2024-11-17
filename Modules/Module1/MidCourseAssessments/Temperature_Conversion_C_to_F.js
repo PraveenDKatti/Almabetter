81ba8b2124eb3758c4d1d06ca0528a3c341dd245
Praveen Katti <praveendk25@gmail.com>
2024-11-17 23:36:58 +0530

    wrote solution

--- Modules/Module1/MidCourseAssessments/Temperature_Conversion_C_to_F.js
+++ Modules/Module1/MidCourseAssessments/Temperature_Conversion_C_to_F.js
@@ -11,3 +11,12 @@ Output:
 Explanation:
 The temperature 25°C converts to (25 * 9/5) + 32 = 77°F. */
 
+// Do not remove nor make any changes in  main() function
+function main() {
+    var celsius = parseInt(readLine());
+    console.log(celsiusToFahrenheit(celsius));
+}
+function celsiusToFahrenheit(celsius) {
+  //  Your code goes here
+  return 9/5*celsius + 32
+}
