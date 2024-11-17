51163ac0406a98396fd81abc09d847ea1ba859eb
Praveen Katti <praveendk25@gmail.com>
2024-11-18 01:29:12 +0530

    added aim of problem and example

---
+++ Modules/Module1/EndCourseAssessments/Valid_Phone_Number.js
@@ -0,0 +1,16 @@
+/* write a JavaScript function that checks if a given string represents a valid phone number.  
+The phone number should: Contain exactly 10 digits. Start with 7, 8, or 9.
+No additional characters (like spaces or hyphens) are allowed. */
+
+/* Example 1:
+Input:
+phoneNumber = "9876543210"
+
+Output:
+true
+
+Explanation:
+The number starts with 9, contains exactly 10 digits, and meets all the validation requirements. */
+
+
+
