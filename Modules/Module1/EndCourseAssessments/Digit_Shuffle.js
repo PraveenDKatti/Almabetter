d2b959491e55be5bda32854eb41516a18cee978a
Praveen Katti <praveendk25@gmail.com>
2024-11-18 17:56:05 +0530

    added aim of problem and example

---
+++ Modules/Module1/EndCourseAssessments/Digit_Shuffle.js
@@ -0,0 +1,12 @@
+/* write a JavaScript function that determines if it’s possible to rearrange the digits of a 
+given positive integer N to create a multiple of 5. */
+
+/* Example 3:
+Input:
+n = 108
+
+Output:
+true
+
+Explanation:
+The digits can be rearranged to form 180, which is a multiple of 5. */
