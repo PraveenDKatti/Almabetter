aa6206ec03975d29afecb714906a1b46ee042094
Praveen Katti <praveendk25@gmail.com>
2024-12-11 01:05:25 +0530

    added aim of problem and example

---
+++ Modules/Module2/MidCourseAssessment/checkEventConflict.js
@@ -0,0 +1,16 @@
+/* Check whether two events, defined by their start and end times in 24-hour format, overlap in time.
+The function should return true if there is any intersection between the two events, 
+indicating a conflict, and false if they do not overlap. */
+
+/* Example 2:
+Input:
+event1 = ["01:00","02:00"], event2 = ["01:20","03:00"]
+
+Output:
+true
+
+Explanation:
+The two events intersect starting from 01:20 to 02:00. */
+
+
+
