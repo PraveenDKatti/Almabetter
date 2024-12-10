ff4e6468a77b4b690a1242a7f5bcecbef7ee4674
Praveen Katti <praveendk25@gmail.com>
2024-12-11 01:14:00 +0530

    added aim of problem and example

---
+++ Modules/Module2/MidCourseAssessment/backspaceStringCompare.js
@@ -0,0 +1,15 @@
+/* Compare two strings after processing backspace operations represented by the '#' character, 
+and determine if the resulting strings are equal. */
+
+
+/* Example 1:
+Input:
+s = "ab#c", t = "ad#c"
+
+Output:
+true
+
+Explanation:
+Both s and t become "ac". */
+
+
