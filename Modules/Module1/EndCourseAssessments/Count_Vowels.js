f8bb48a9bad0b48247cb25b584733880601093ff
Praveen Katti <praveendk25@gmail.com>
2024-12-11 00:41:33 +0530

    wrote solution

--- Modules/Module1/EndCourseAssessments/Count_Vowels.js
+++ Modules/Module1/EndCourseAssessments/Count_Vowels.js
@@ -12,3 +12,10 @@ Explanation:
 The vowels in the string are e, o, and o, totaling 3 vowels. */
 
 
+function countVowels(str) {
+    //matching the vowels [a,e,i,o,u] using regular expression with insensitive flag
+   let match = str.match(/[aeiou]/ig)
+   
+   //if there is a match return count of matched Vowels, if match is Null returns 0
+   return match?match.length:0
+ }
