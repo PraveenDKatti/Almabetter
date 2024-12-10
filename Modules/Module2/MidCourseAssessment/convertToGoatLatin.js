9c3258051a817c9c77d994f039657b0c6b9be318
Praveen Katti <praveendk25@gmail.com>
2024-12-11 01:20:54 +0530

    added aim of problem and example

---
+++ Modules/Module2/MidCourseAssessment/convertToGoatLatin.js
@@ -0,0 +1,21 @@
+/* Transform a given sentence into "Goat Latin" by:
+
+Appending "ma" to words starting with vowels.
+Moving the first letter to the end and appending "ma" for words starting with consonants.
+Adding 'a' repeatedly based on the word's position in the sentence.
+Return the modified sentence as the result. */
+
+/* Example 1:
+Input:
+sentence = "I speak Goat Latin"
+
+Output:
+"Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+
+Explanation:
+"I" starts with a vowel → "Imaa"
+"speak" starts with a consonant → "peaksma"
+"Goat" starts with a consonant → "oatGma"
+"Latin" starts with a consonant → "atinLma" */
+
+
