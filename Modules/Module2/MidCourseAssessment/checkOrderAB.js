ba7977fc5472592a13eea74e81412ab05943b14e
Praveen Katti <praveendk25@gmail.com>
2024-12-11 00:59:59 +0530

    wrote solution

--- Modules/Module2/MidCourseAssessment/checkOrderAB.js
+++ Modules/Module2/MidCourseAssessment/checkOrderAB.js
@@ -12,3 +12,16 @@ Explanation:
 The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5. Hence, 
 every 'a' appears before every 'b' and we return true. */
 
+var checkString = function(s) {
+    // Your code goes here
+    let appear = false
+    for(let c of s){
+      if(c === 'B'){
+          appear = true
+      }
+      else if(c === 'A' && appear){
+          return false
+    }
+    }
+    return true
+  };
