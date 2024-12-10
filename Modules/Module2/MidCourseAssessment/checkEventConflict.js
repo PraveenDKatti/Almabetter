a1f63ab547a89d9d0b79d334dd61e37ed8b9a0fb
Praveen Katti <praveendk25@gmail.com>
2024-12-11 01:06:08 +0530

    wrote solution

--- Modules/Module2/MidCourseAssessment/checkEventConflict.js
+++ Modules/Module2/MidCourseAssessment/checkEventConflict.js
@@ -13,4 +13,16 @@ Explanation:
 The two events intersect starting from 01:20 to 02:00. */
 
 
-
+var haveConflict = function(event1, event2) {
+    // Your code goes here
+    let [endH1,endM1] = event1[1].split(":")
+    let [startH2,startM2] = event2[0].split(":")
+    
+    if(startH2<=endH1){
+        if(endH1===startH2){
+            return endM1 >= startM2
+        }
+        return true
+    }
+    return false
+};
