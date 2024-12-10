02e7ea181a81d8a9c1bbbb3cd4cf93f80a955430
Praveen Katti <praveendk25@gmail.com>
2024-12-11 00:59:20 +0530

    added aim of problem and example

---
+++ Modules/Module2/MidCourseAssessment/checkOrderAB.js
@@ -0,0 +1,14 @@
+/* Write a function to check whether all 'A's in a given string appear before all 'B's, 
+returning true if the condition is satisfied and false otherwise. */
+
+/* Example 1:
+Input:
+s = "aaabbb"
+
+Output:
+true
+
+Explanation:
+The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5. Hence, 
+every 'a' appears before every 'b' and we return true. */
+
