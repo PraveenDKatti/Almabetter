5b246a61bbc0722c92455b7f0b16a937edc787d5
Praveen Katti <praveendk25@gmail.com>
2024-12-11 01:24:12 +0530

    added aim of problem and example

---
+++ Modules/Module2/MidCourseAssessment/minimumIndexSum.js
@@ -0,0 +1,15 @@
+/* Find common dish names in two lists with the smallest index sum, and return all such dishes 
+in any order if multiple have the same minimum sum. */
+
+/* Example 1:
+Input:
+list1 = ["Shogun","Tapioca Express","Burger King","KFC"], 
+list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+
+Output:
+["Shogun"]
+
+Explanation:
+The only common string is "Shogun". */
+
+
