0be6ca3542ca50dfa0a3d2abcd8a68df43e548fc
Praveen Katti <praveendk25@gmail.com>
2024-12-11 00:52:10 +0530

    added aim of problem and example

---
+++ Modules/Module2/MidCourseAssessment/Intersection_of_Two_Arrays.js
@@ -0,0 +1,13 @@
+/* write a Javascript function that returns array of common elements of two arrays, 
+including duplicates, based on their frequency in both arrays. */
+
+/* Example 2:
+Input:
+nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+
+Output:
+[4,9]
+
+Explanation:
+Both 4 and 9 appear in both arrays, so they are included in the intersection. */
+
