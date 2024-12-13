7c07972d84d9391016e59888213206dd646d2f97
Praveen Katti <praveendk25@gmail.com>
2024-12-13 18:38:34 +0530

    added aim of problem and example

---
+++ Modules/Module2/MidCourseAssessment/PositionsOfLargeGroups.js
@@ -0,0 +1,13 @@
+/* Find all groups of consecutive identical characters in a string s that have a size of 3 or more, 
+and return their starting and ending indices as intervals, sorted by the starting index. */
+
+/* Example 3:
+Input:
+s = "abcdddeeeeaabbbcd"
+
+Output:
+[[3,5],[6,9],[12,14]]
+
+Explanation:
+The large groups are "ddd", "eeee", and "bbb". */
+
