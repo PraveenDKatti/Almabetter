2552b9879f4aaca3fc4f72b10143f149119911d1
Praveen Katti <praveendk25@gmail.com>
2024-12-13 18:35:14 +0530

    wrote solution

--- Modules/Module2/MidCourseAssessment/miceAndCheese.js
+++ Modules/Module2/MidCourseAssessment/miceAndCheese.js
@@ -12,3 +12,19 @@ Explanation:
 In this example, the first mouse eats the 2nd (0-indexed) and the 3rd types of cheese, 
 and the second mouse eats the 0th and the 1st types of cheese. The total points are 4 + 4 + 3 + 4 = 15. 
 It can be proven that 15 is the maximum total points that the mice can achieve. */
+
+
+var miceAndCheese = function(reward1, reward2, k) {
+    // Your code goes here
+    let maxPoints = 0
+    let rewards = []
+    for(let i=0; i<reward1.length; i++){
+        rewards.push([reward1[i],i])
+    }
+    rewards.sort((a,b)=>b[0]-a[0]).slice(0,k)
+    for(let i=0;i<reward1.length; i++){
+        if(i<k) maxPoints +=reward1[rewards[i][1]]
+        else maxPoints +=reward2[rewards[i][1]]
+    }
+    return maxPoints
+};
