8eff44b8301f0174bec628cb0f9718f1e4cff342
Praveen Katti <praveendk25@gmail.com>
2024-12-13 18:42:39 +0530

    added aim of problem and example

---
+++ Modules/Module2/MidCourseAssessment/GreatestSumDivisibleByThree.js
@@ -0,0 +1,14 @@
+/* Determine the maximum sum of a subset of integers from a given array such that the sum is 
+divisible by three, using an efficient approach to evaluate possible combinations. */
+
+/* Example 1:
+Input:
+nums = [3,6,5,1,8]
+
+Output:
+18
+
+Explanation:
+Pick numbers 3, 6, 1 and 8 their sum is 18 (maximum sum divisible by 3). */
+
+
