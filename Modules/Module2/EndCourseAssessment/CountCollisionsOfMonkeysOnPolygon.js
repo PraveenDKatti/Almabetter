247f1838ece1fa4b185f54c238cf297f12842ce5
Praveen Katti <praveendk25@gmail.com>
2025-01-30 14:53:49 +0530

    added aim of Problem and example

---
+++ Modules/Module2/EndCourseAssessment/CountCollisionsOfMonkeysOnPolygon.js
@@ -0,0 +1,19 @@
+/* To determine the number of movement configurations for monkeys on a regular convex polygon that 
+result in at least one collision, where monkeys move to neighboring vertices. 
+The result should be returned modulo 10^9 + 7. */
+
+/* Example 1:
+Input:
+3
+
+Output:
+6
+
+Explanation:
+There are 8 total possible movements. Two ways such that they collide at some point are:
+Monkey 1 moves in a clockwise direction; monkey 2 moves in an anticlockwise direction; 
+monkey 3 moves in a clockwise direction. Monkeys 1 and 2 collide.
+Monkey 1 moves in an anticlockwise direction; monkey 2 moves in an anticlockwise direction; 
+monkey 3 moves in a clockwise direction. Monkeys 1 and 3 collide. 
+It can be shown 6 total movements result in a collision. */
+
