9fd69bb30e3496a2f4d69ea73186d8f6c04e40cd
Praveen Katti <praveendk25@gmail.com>
2025-01-30 15:01:58 +0530

    added aim of Problem and example

---
+++ Modules/Module2/EndCourseAssessment/PredictTheWinner.js
@@ -0,0 +1,18 @@
+/* To determine if Player 1 can win a game by selecting numbers from either end of an integer array, 
+given both players play optimally. The function should return true if Player 1 can achieve a 
+score greater than or equal to Player 2's score after all elements are picked. */
+
+
+/* Example 2:
+Input:
+[1,5,233,7]
+
+Output:
+true
+
+Explanation:
+Player 1 first chooses 1. Then player 2 has to choose between 5 and 7. 
+No matter which number player 2 choose, player 1 can choose 233. Finally, 
+player 1 has more score (234) than player 2 (12), so you need to return 
+True representing player1 can win. */
+
