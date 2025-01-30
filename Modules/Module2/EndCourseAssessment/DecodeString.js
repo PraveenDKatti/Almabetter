ce9bfbd1019a274ea47b5fb89bbaa680d150e182
Praveen Katti <praveendk25@gmail.com>
2025-01-30 14:58:05 +0530

    added aim of Problem and example

---
+++ Modules/Module2/EndCourseAssessment/DecodeString.js
@@ -0,0 +1,15 @@
+/* To implement a function that decodes a given encoded string following the format k[encoded_string], 
+where encoded_string is repeated k times. The function should handle valid inputs with well-formed 
+square brackets and return the fully decoded string. */
+
+
+/* Example 2:
+Input:
+s= "3[a2[c]]"
+
+Output:
+accaccacc
+
+Explanation:
+The string can be decoded by first decoding "a2[c]" to "acc", and then repeating that three times. */
+
