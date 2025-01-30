889b5b53fef85c3ea6765f781309ac29797452ec
Praveen Katti <praveendk25@gmail.com>
2025-01-30 14:49:01 +0530

    added aim of Problem and example

---
+++ Modules/Module2/EndCourseAssessment/RegularExpressionMatching.js
@@ -0,0 +1,15 @@
+/* To implement a function that checks if a given string matches a specified pattern using special 
+characters like . (matching any single character) and * (matching zero or more of the preceding element).
+The function should accurately identify valid matches based on these criteria. */
+
+/* Example 2:
+Input:
+s = "aa", p = "a*"
+
+Output:
+true
+
+Explanation:
+'*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa". */
+
+
