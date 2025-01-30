d9f30cc80de906888ec07b5e11bef512d21b43e4
Praveen Katti <praveendk25@gmail.com>
2025-01-30 14:58:44 +0530

    wrote solution

--- Modules/Module2/EndCourseAssessment/DecodeString.js
+++ Modules/Module2/EndCourseAssessment/DecodeString.js
@@ -13,3 +13,33 @@ accaccacc
 Explanation:
 The string can be decoded by first decoding "a2[c]" to "acc", and then repeating that three times. */
 
+
+var decodeString = function(s) {
+	// Your code goes here
+      let stack = [];
+    let currentString = '';
+    let currentNumber = 0;
+
+    for (let char of s) {
+        if (!isNaN(char)) {
+            // If it's a number, build the current number
+            currentNumber = currentNumber * 10 + Number(char);
+        } else if (char === '[') {
+            // Push the current number and string onto the stack
+            stack.push(currentString);
+            stack.push(currentNumber);
+            currentString = '';
+            currentNumber = 0;
+        } else if (char === ']') {
+            // Pop the number and previous string from the stack
+            let repeatTimes = stack.pop();
+            let previousString = stack.pop();
+            currentString = previousString + currentString.repeat(repeatTimes);
+        } else {
+            // Append the character to the current string
+            currentString += char;
+        }
+    }
+
+    return currentString;
+}
