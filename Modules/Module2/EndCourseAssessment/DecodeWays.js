29327fb8697dda43c85d26af38bb3ba24b0ec687
Praveen Katti <praveendk25@gmail.com>
2025-01-30 14:27:03 +0530

    added aim of Problem and example

---
+++ Modules/Module2/EndCourseAssessment/DecodeWays.js
@@ -0,0 +1,14 @@
+/* To determine the number of valid ways to decode a string of digits into letters based on a given mapping, 
+where each digit or combination of digits represents a letter from 'A' to 'Z'. 
+The solution must account for invalid groupings that do not correspond to valid letters. */
+
+/* Example 2:
+Input:
+s = "226"
+
+Output:
+3
+
+Explanation:
+"226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6). */
+
