72f7e9f07a09c99d5dcd4104422d5971159746be
Praveen Katti <praveendk25@gmail.com>
2025-01-30 14:34:55 +0530

    added aim of Problem and example

---
+++ Modules/Module2/EndCourseAssessment/IntegerToRoman.js
@@ -0,0 +1,14 @@
+/* To convert a given integer into its equivalent Roman numeral representation by applying the standard mappings of integers to Roman symbols. 
+The conversion must accurately follow the rules of Roman numeral formation, 
+including the use of subtraction for specific values (e.g., 4 as IV and 9 as IX) to ensure correctness in representation. */
+
+/* Example 2:
+Input:
+num = 58
+
+Output:
+"LVIII"
+
+Explanation:
+L = 50, V = 5, III = 3. */
+
